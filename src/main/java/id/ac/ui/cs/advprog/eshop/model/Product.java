d10e0eea7a57fff3cf57de27351ace5a49f8aef7
Anisa Maharani <anismyes@gmail.com>
2024-02-04 00:31:47 +0800

    Create Product model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String productId;
+    private String peoductName;
+    private int productQuantity;
+}
