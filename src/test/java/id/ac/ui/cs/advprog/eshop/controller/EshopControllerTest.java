8c807b94211962baa21e5759aee4d0924c0f66d1
Anisa Maharani <anismyes@gmail.com>
2024-02-10 15:28:54 +0800

    [TUTORIAL 2] Create Eshop Controller unit test

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/EshopControllerTest.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = EshopController.class)
+public class EshopControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testProductList() throws Exception {
+
+        mockMvc.perform(get(""))
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("homePage"))
+                .andExpect(view().name("home"));
+    }
+
+}
