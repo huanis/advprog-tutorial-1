468c8835f48fe93caa2b5220efe4cc8eda3e3d52
Anisa Maharani <anismyes@gmail.com>
2024-02-14 10:10:59 +0800

    [TUTORIAL 2] Fix code smell: remove public modifier ProductServiceImpl

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -16,7 +16,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-public class ProductServiceImplTest {
+class ProductServiceImplTest {
 
     @Mock
     ProductRepository productRepository;
