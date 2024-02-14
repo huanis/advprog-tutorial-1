a8a9fbb51d70643bea44b32f5edf2559ba2d755d
Anisa Maharani <anismyes@gmail.com>
2024-02-14 10:14:35 +0800

    [TUTORIAL 2] Fix code smell: remove public modifier ProductControllerTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = ProductController.class)
-public class ProductControllerTest {
+class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
